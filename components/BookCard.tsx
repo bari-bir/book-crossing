fbff0cc302a5f8286f5e8ee4de839079944da2a6
yrysNM <yrys.n3m@gmail.com>
2024-01-16 23:25:02 +0600

    feat: add prettier and started style

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -1,38 +1,28 @@
-import styled from "styled-components/native";
+import { View } from "react-native"
+import styled from "styled-components/native"
 
 const BookWrapper = styled.Text`
-  padding: 12px;
-  /* box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px; */
-  /* display: flex; */
-  align-items: center;
-  gap: 5px;
-`;
+    padding: 10px 11px 5px 11px;
+    border-radius: 5px;
+    background: #f1f1f1;
+`
 
-const BookTitle = styled.Text`
-  color: #000;
-  font-family: Inter;
-  font-size: 14px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
-`;
-
-const BookImage = styled.Image`
-  width: 50px;
-  height: 50px;
-  object-fit: contain;
-  object-position: center;
-`;
+const BookImage = styled.ImageBackground`
+    width: 133px;
+    height: auto;
+    object-fit: contain;
+    border-radius: 5px;
+`
 
 export const BookCard = () => {
-  return (
-    <BookWrapper>
-      <BookImage
-        source={{
-          uri: "https://img.freepik.com/free-photo/book-composition-with-open-book_23-2147690555.jpg",
-        }}
-      />
-      <BookTitle />
-    </BookWrapper>
-  );
-};
+    return (
+        <BookWrapper>
+            <BookImage
+                source={{
+                    uri: "@expo/assets/book-test.png",
+                }}>
+                <View style={{ width: "100%", height: "100%", position: "absolute", backgroundColor: "lightgray 50%" }} />
+            </BookImage>
+        </BookWrapper>
+    )
+}
