08c95b9fb0b7f95dc42d49c3a3310ea1443d592d
yrysNM <yrys.n3m@gmail.com>
2024-02-06 19:25:45 +0600

    feat: add message card block

--- src/screens/Message.tsx
+++ src/screens/Message.tsx
@@ -1,8 +1,9 @@
-import { View, Image, TextInput, StyleSheet } from "react-native"
+import { View, Image, TextInput, StyleSheet, Text } from "react-native"
 import { Page } from "../layouts/page"
 import { Header } from "../components/Header"
-import { useState } from "react"
+import React, { useState } from "react"
 import SearchImage from "../../assets/images/search.png"
+import ProfileImage from "../../assets/images/profile.png"
 
 export const Message = () => {
     const [search, setSearch] = useState<string>("")
@@ -12,6 +13,22 @@ export const Message = () => {
             <View style={styles.search}>
                 <TextInput onChangeText={setSearch} value={search} placeholder="Search..." style={{ paddingLeft: 21 }} />
                 <Image source={SearchImage} style={styles.searchImg} />
+            </View>
+
+            <View style={styles.contentBlock}>
+                <View style={styles.contentWrapper}>
+                    <Image style={styles.userProfileImage} source={ProfileImage} />
+                    <View>
+                        <Text>Akbota Test</Text>
+                        <Text>Want to change the book!</Text>
+                    </View>
+                </View>
+                <View>
+                    <Text>13:44</Text>
+                    <View>
+                        <Text>1</Text>
+                    </View>
+                </View>
             </View>
         </Page>
     )
@@ -25,11 +42,34 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         display: "flex",
         justifyContent: "center",
+        marginBottom: 14,
     },
     searchImg: {
         position: "absolute",
         right: 5,
         width: 22,
         height: 22,
+    },
+    contentBlock: {
+        paddingHorizontal: 12,
+        paddingVertical: 16,
+        backgroundColor: "#D2D2D2",
+        borderRadius: 5,
+        height: 97,
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+    contentWrapper: {
+        display: "flex",
+        gap: 13,
+        alignItems: "center",
+        flexDirection: "row",
+    },
+    userProfileImage: {
+        width: 65,
+        height: 65,
+        borderRadius: 100,
     },
 })
