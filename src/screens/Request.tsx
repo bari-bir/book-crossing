5419cda95fda445c78344665cc1eca67d109de43
yrysNM <yrys.n3m@gmail.com>
2024-02-07 22:55:50 +0600

    feat: change style in Request page

--- src/screens/Request.tsx
+++ src/screens/Request.tsx
@@ -1,15 +1,45 @@
-import { View, Text } from "react-native"
-import React from "react"
+import { View, Text, StyleSheet } from "react-native"
+import React, { useState } from "react"
 import { Page } from "../layouts/page"
 import { Header } from "../components/Header"
 
 export const Request = () => {
+    const [tabInfo, setTabInfo] = useState<{ type: string }>({ type: "request" })
     return (
         <Page>
             <Header title={"Request"} isBack={false} />
-            <View>
-                <Text>HELLO WORLD</Text>
+            <View style={{ ...styles.requestTabs }}>
+                <Text>Requests</Text>
+                <Text>Announcement</Text>
             </View>
         </Page>
     )
 }
+
+const styles = StyleSheet.create({
+    requestTabs: {
+        marginTop: 20,
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingBottom: 7,
+
+        /**
+         * @TODO add tab for request and announcement
+         */
+        "&::before": {
+            content: "",
+            position: "absolute",
+            top: 0,
+            display: "flex",
+            flex: 1,
+            width: "100%",
+            height: 1,
+            backgroundColor: "#C2BEBE",
+        },
+    },
+    requestTab: {
+        width: "50%",
+    },
+})
