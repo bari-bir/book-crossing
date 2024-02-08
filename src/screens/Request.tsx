afa4e746b6406f0d4f76604d203035230b563393
yrysNM <yrys.n3m@gmail.com>
2024-02-09 00:49:43 +0600

    feat: add block for request

--- src/screens/Request.tsx
+++ src/screens/Request.tsx
@@ -1,7 +1,8 @@
-import { View, Text, StyleSheet } from "react-native"
+import { View, Text, StyleSheet, Image } from "react-native"
 import React, { useState } from "react"
 import { Page } from "../layouts/page"
 import { Header } from "../components/Header"
+import BookImage from "../../assets/images/book-test.png"
 
 export const Request = () => {
     const [tabInfo, setTabInfo] = useState<{ type: string }>({ type: "request" })
@@ -11,6 +12,17 @@ export const Request = () => {
             <View style={{ ...styles.requestTabs }}>
                 <Text>Requests</Text>
                 <Text>Announcement</Text>
+            </View>
+
+            <View style={styles.requestBlock}>
+                <Image style={styles.requestBookImg} source={BookImage} />
+
+                <View style={styles.requestInfo}>
+                    <Text>Akbota Zhumakhanbet</Text>
+                    <Text>The standard Lorem Ipsum passage, used since "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor ...</Text>
+
+                    {/* ADD BUTTONS */}
+                </View>
             </View>
         </Page>
     )
@@ -41,5 +53,25 @@ const styles = StyleSheet.create({
     },
     requestTab: {
         width: "50%",
+    },
+    requestBlock: {
+        marginTop: 13,
+        paddingVertical: 8,
+        paddingHorizontal: 6,
+        borderRadius: 5,
+        height: 100,
+        backgroundColor: "#F6F5F5",
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "row",
+        justifyContent: "space-around",
+        gap: 10,
+    },
+    requestBookImg: {
+        height: 85,
+        width: 85,
+    },
+    requestInfo: {
+        flex: 1,
     },
 })
