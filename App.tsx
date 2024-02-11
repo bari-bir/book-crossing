082e792c58ec0631ea8425fa215a5fc7058b6e4f
yrysNM <yrys.n3m@gmail.com>
2024-02-11 19:27:48 +0600

    feat(App): add Stack.screen

--- App.tsx
+++ App.tsx
@@ -1,5 +1,16 @@
 import { MainNavigation } from "./src/navigation/MainNavigation"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { BookDetail } from "./src/screens/BookDetail"
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
-    return <MainNavigation />
+    return (
+        <>
+            <Stack.Navigator initialRouteName={"Home"}>
+                <Stack.Screen name={"BookDetail"} component={BookDetail} />
+            </Stack.Navigator>
+            <MainNavigation />
+        </>
+    )
 }
