f385bdab0c1c0fcba47d3db7560074a39f572f1a
yrysNM <yrys.n3m@gmail.com>
2024-02-14 23:45:27 +0600

    feat: add navigation without type

--- src/screens/NewsPage.tsx
+++ src/screens/NewsPage.tsx
@@ -2,8 +2,11 @@ import { BookList } from "../components/BookList"
 import { BookCard } from "../components/BookCard"
 import { BookListHeader } from "../components/BookListHeader"
 import { Page } from "../layouts/page"
+import { NavigationContainer } from "@react-navigation/native"
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
 
-export const NewsPage = () => {
+export const NewsPage = ({ navigation }) => {
+    console.log(navigation)
     return (
         <Page>
             <BookListHeader />
