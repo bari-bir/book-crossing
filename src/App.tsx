4a412cec0957d3a90e444948b73fa59cb334d363
yrysNM <yrys.n3m@gmail.com>
2024-03-06 18:59:08 +0500

    feat: add and

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,12 @@
-import {useEffect} from "react";
-import axios from "axios";
+import {Route, Routes} from "react-router-dom";
+import {Home} from "./pages/Home.tsx";
 
 function App() {
-    useEffect(() => {
-        testRequest();
-    }, []);
-
-    const testRequest = async () => {
-      const {token} = JSON.parse(localStorage.getItem('token') || "");
-        await axios.post("http://192.168.1.108:8080/bookcrossing/request/list",{}, {
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${token}`
-            }
-        }).then(res => {
-            alert(JSON.stringify(res.data));
-        }).catch(err =>  {
-            alert(err);
-        })
-    }
-
   return (
     <div style={{ height: "100%", width: "100%" }}>
-      <h1>Test</h1>
-      <button
-        onClick={() => {
-          window.history.back();
-        }}
-      >
-        Back
-      </button>
+        <Routes>
+            <Route path="/"  element={<Home />} />
+        </Routes>
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
+import {Input} from "antd";
+import {useState} from "react";
+
+
+const { Search } = Input;
 export const Home = () => {
+    const [search, setSearch] = useState<string>("")
+
     return (
         <div>
-            Hello WORLD
+            <Search placeholder="Поиск в Алматы " allowClear value={search} onChange={(e) => setSearch(e.target.value)}/>
         </div>
     )
 }
+
