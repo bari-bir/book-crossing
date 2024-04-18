fa966834a02e519b4497542b0f3c08d682fe7922
yrysNM <yrys.n3m@gmail.com>
2024-04-18 19:24:22 +0500

    redisign

--- src/components/TabbarMenu.tsx
+++ src/components/TabbarMenu.tsx
@@ -6,20 +6,36 @@ export const TabbarMenu = () => {
     const location = useLocation()
     return (
         <div className="tabbar">
+            <div className="tabbar-animation"></div>
             <NavLink to="/" className={({ isActive }) => (isActive || location.pathname.indexOf("book-exchange") !== -1 ? "active-link" : "")}>
-                <ReadOutlined  className="menu-icon" />
+                <div className="tabbar-block">
+                    <ReadOutlined className="menu-icon" />
+                    <p className="tabbar-text">Books</p>
+                </div>
             </NavLink>
             <NavLink to="/favorute" className={({ isActive }) => (isActive ? "active-link" : "")}>
-                <HeartOutlined className="menu-icon" />
+                <div className="tabbar-block">
+                    <HeartOutlined className="menu-icon" />
+                    <p className="tabbar-text">Likes</p>
+                </div>
             </NavLink>
             <NavLink to="/create-announcement/add" className={({ isActive }) => (isActive ? "active-link" : "")}>
-                <PlusCircleOutlined className="menu-icon" />
+                <div className="tabbar-block">
+                    <PlusCircleOutlined className="menu-icon" />
+                    <p className="tabbar-text">Create</p>
+                </div>
             </NavLink>
             <NavLink to="/message" className={({ isActive }) => (isActive ? "active-link" : "")}>
-                <MessageOutlined className="menu-icon" />
+                <div className="tabbar-block">
+                    <MessageOutlined className="menu-icon" />
+                    <p className="tabbar-text">Messages</p>
+                </div>
             </NavLink>
             <NavLink to="/request-annoucement" className={({ isActive }) => (isActive ? "active-link" : "")}>
-                <BookOutlined className="menu-icon" />
+                <div className="tabbar-block">
+                    <BookOutlined className="menu-icon" />
+                    <p className="tabbar-text">Reques</p>
+                </div>
             </NavLink>
         </div>
     )
